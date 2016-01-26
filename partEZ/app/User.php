1d7580995f3100c99798e93213ccced9b4e027be
Delroy <delroy.hiebert@gmail.com>
2016-01-25 19:37:05 -0600

    changed user auth model to fit to database design

--- partEZ/app/User.php
+++ partEZ/app/User.php
@@ -11,8 +11,9 @@ class User extends Authenticatable
      *
      * @var array
      */
+    protected $primaryKey = 'uid';
     protected $fillable = [
-        'name', 'email', 'password',
+        'firstname', 'lastname', 'email', 'password',
     ];
 
     /**
