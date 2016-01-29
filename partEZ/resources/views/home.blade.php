d4dcb17d3cedff91a95a3096a6ccf4bb37253fef
Greg Joubert <gregjoubert@wpa-3-354.cc.umanitoba.ca>
2016-01-29 13:19:04 -0600

    merged with most recent but form

--- partEZ/resources/views/home.blade.php
+++ partEZ/resources/views/home.blade.php
@@ -9,17 +9,13 @@
 
                 <div class="panel-body">
                     You are logged in!
-<<<<<<< HEAD
+
 
                    <!-- <a href="{{ url('/success') }}" role="button">Send Email</a> -->
 
                     <br><br>
-                    <button href="{{ route('email') }}" type="button" class="btn btn-default">Send Email</button>
-                    <br><br>
                     <button type="button" onclick="window.location='{{ url("/create_event") }}'">Create a new Event</button>
 
-=======
->>>>>>> c06b5f81168e63e42ac6d547356882faa9008d89
                 </div>
             </div>
         </div>
