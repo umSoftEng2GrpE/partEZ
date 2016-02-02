7de1248cb9d3b2976b151323a1c143c48f360c9f
nickmanaigre <nickmanaigre@gmail.com>
2016-02-02 16:24:23 -0600

    add some event display stuff

--- partEZ/database/factories/ModelFactory.php
+++ partEZ/database/factories/ModelFactory.php
@@ -20,3 +20,13 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Event::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'location' => $faker->name,
+        'date' => $faker->email,
+        'stime' => str_random(10),
+        'etime' => str_random(10),
+    ];
+});
