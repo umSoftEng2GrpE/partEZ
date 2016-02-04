61c30c628f941738cc265711d94f0306355af39d
Greg Joubert <umjoubeg@myumanitoba.ca>
2016-02-04 15:59:36 -0600

    generic post form for poll

--- partEZ/resources/views/polls/poll_vote.blade.php
+++ partEZ/resources/views/polls/poll_vote.blade.php
@@ -1,8 +1,17 @@
 
 @if (count($options))
-    @foreach($options as $option)
-        <p> {{ $option['option'] }} </p>
-    @endforeach
+    <html>
+    <br>
+    <form method="post" action="/submit_poll">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input type="hidden" name="pid" value="<?php echo $options[0]->pid ?>" id="<?php echo $options[0]->pid ?>" >
+        <div class=":poll-options">
+
+        </div>
+
+        <input type="submit" value="Submit Vote">
+    </form>
 @else
     <p>This event has no polls.</p>
 @endif
+
