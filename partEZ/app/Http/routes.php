74ce60e5af1bf2e6d6f743b6c1b53ee6deee5869
maxewaschuk <ewaschukmps@gmail.com>
2016-02-03 19:04:55 -0600

    Attempting to add invite acceptance to routes.

--- partEZ/app/Http/routes.php
+++ partEZ/app/Http/routes.php
@@ -38,4 +38,5 @@ Route::group(['middleware' => 'web'], function () {
     Route::post('event/{id}', ['as' => 'events.event_details', 'uses' => 'EventController@details']);
     Route::post('polls/{polls}', ['as' => 'polls.poll_options', 'uses' => 'EventController@details']);
     Route::post('create_poll', 'EventController@validatePoll');
+    Route::get('accept_invite/{eid}/{uid}', ['as' => 'events.accept', 'uses' => 'EventController@inviteAccept']);
 });
