01007424316175a6bce29ed9f43bb7e637778adc
maxewaschuk <ewaschukmps@gmail.com>
2016-02-09 14:10:35 -0600

    Added message model and accessor methods.

---
+++ partEZ/app/Message.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+
+class Message extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'messages';
+
+    protected $primaryKey = 'mid';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'message', 'eid', 'uid',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $guarded = [
+        'mid',
+    ];
+
+
+    // Gets all messages for the specified event.
+    public static function getByAllByEid($eid) 
+    {
+    	return DB::table('message')->where('eid', $eid);
+    }
+
+
+    // Gets all messages for the specified user.
+    public static function getAllByUid($uid)
+    {
+    	return DB::table('message')->where('uid', $uid);
+    }
+
+
+    // Gets all messages for the specified event after the specified message.
+    public static function getAllRecent($eid, $mid)
+    {
+    	return DB::table('message')->where('eid', $eid)->where('mid', '>', $mid);
+    }
+
+}
