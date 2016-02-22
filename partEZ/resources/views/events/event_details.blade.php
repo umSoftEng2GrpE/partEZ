b74423725567be1cedf5b88a9581c612bb6fef0f
Delroy Hiebert <delroy.hiebert@gmail.com>
2016-02-21 21:41:26 -0600

    tabbed event details

--- partEZ/resources/views/events/event_details.blade.php
+++ partEZ/resources/views/events/event_details.blade.php
@@ -5,41 +5,57 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Dashboard</div>
+				<div class="panel-heading"><h3> {{ $event['name'] }} </h3></div>
 
-				<div class="panel-body">
-					<h3> Event Name: {{ $event['name'] }} </h3>
-					<br>
-					<ul>
-						<li> Date:{{ $event['date'] }} </li>
-						<li> Start Time: {{ $event['stime']}} End Time: {{ $event['etime']}} </li>
-						<li> Location: {{ $event['location'] }} </li>
-						<li> Description: {{ $event['description'] }} </li>
-					</ul>
+				<ul class="nav nav-tabs" id="myTabs">
+                    <li  ><a href="#tab1" data-toggle="tab">Event Description</a></li>
+                    <li><a href="#tab2" data-toggle="tab">Event Poll</a></li>
+                    <li><a href="#tab3" data-toggle="tab">Items List</a></li>
+                    <li><a href="#tab4" data-toggle="tab">People</a></li>
+                    <li><a href="#tab5" data-toggle="tab">Fifth</a></li>
+                    <li><a href="#tab6" data-toggle="tab">Sixth</a></li>
+                    <li><a href="#tab7" data-toggle="tab">Seventh</a></li>
+                </ul>
 
-					<h4>Polls</h4>
-					@if (count($all_options))
-					@foreach($all_options as $options)
-					@include('polls.poll_display', $options )
-					@endforeach
-					@else
-					<p>This event has no polls.</p>
-					@endif
-
-					<h4>Invited</h4>
-					<div>
-						@if (count($invites))
+                <div class="tab-content">
+                	<div class="tab-pane" id="tab1">
+                		
+						<br>
 						<ul>
-							@foreach($invites as $person)
-							<li>{{print_r($person, true)}}</li>
-							@endforeach
+							<li> Date:{{ $event['date'] }} </li>
+							<li> Start Time: {{ $event['stime']}} End Time: {{ $event['etime']}} </li>
+							<li> Location: {{ $event['location'] }} </li>
+							<li> Description: {{ $event['description'] }} </li>
 						</ul>
+                	</div>
+                	<div class="tab-pane" id="tab2">
+                		<h4>Polls</h4>
+						@if (count($all_options))
+						@foreach($all_options as $options)
+						@include('polls.poll_display', $options )
+						@endforeach
 						@else
-						<p>This event has no invitees.</p>
+						<p>This event has no polls.</p>
 						@endif
-					</div>
-
-				</div>
+                	</div>
+                	<div class="tab-pane" id="tab3">
+                		<h4>Items list</h4>
+                	</div>
+                	<div class="tab-pane" id="tab4">
+                		<h4>Invited</h4>
+						<div>
+							@if (count($invites))
+							<ul>
+								@foreach($invites as $person)
+								<li>{{print_r($person, true)}}</li>
+								@endforeach
+							</ul>
+							@else
+							<p>This event has no invitees.</p>
+							@endif
+						</div>
+                	</div>
+               	</div>
 			</div>
 		</div>
 	</div>
