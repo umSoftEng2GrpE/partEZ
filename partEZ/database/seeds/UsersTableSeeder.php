d48e90c1372c686009a70244fe45830c6b74d974
nickmanaigre <nickmanaigre@gmail.com>
2016-02-25 15:04:10 -0600

    add another Selenium test
    In this new test, one user creates a public event, logs out,
    then another user logs in to view it. This is to test the
    public events user story.

--- partEZ/database/seeds/UsersTableSeeder.php
+++ partEZ/database/seeds/UsersTableSeeder.php
@@ -11,7 +11,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $this->addTestUser();
+        $this->addTestUsers();
         // create some random users
         factory(App\User::class, 10)->create();
     }
@@ -19,12 +19,19 @@ class UsersTableSeeder extends Seeder
     /*
      * Add a user for the Selenium Tests
      */
-    private function addTestUser()
+    private function addTestUsers()
     {
         DB::table('users')->insert([
             'firstname' => 'test',
             'lastname' => 'user',
             'email' => 'partEZSelenium'.'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'firstname' => 'test',
+            'lastname' => 'user2',
+            'email' => 'partEZSelenium2'.'@gmail.com',
             'password' => bcrypt('secret'),
         ]);
     }
