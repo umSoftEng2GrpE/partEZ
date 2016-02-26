6194de291d77a4369193ba38aeb3739bbb578a08
Greg Joubert <umjoubeg@myumanitoba.ca>
2016-02-26 17:56:22 -0600

    fixed spacing on vote for poll for invitees

--- partEZ/resources/views/polls/poll_vote.blade.php
+++ partEZ/resources/views/polls/poll_vote.blade.php
@@ -4,7 +4,6 @@
     <br>
     <form method="post" action="/submit_poll">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
         <input type="hidden" name="pid" value="{!! $options[key(reset($options))]->pid !!}" >
         <div class=":poll-options">
             <?php foreach($options as $index => $option): ?>
