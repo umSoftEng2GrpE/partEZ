c9c693ef0fb3bcd33c852306e12ee2de0078ced8
Greg Joubert <umjoubeg@myumanitoba.ca>
2016-02-26 19:50:38 -0600

    updated view to reflect tabs

--- partEZ/resources/views/polls/poll_vote.blade.php
+++ partEZ/resources/views/polls/poll_vote.blade.php
@@ -1,23 +1,18 @@
-
-@if (count($options))
-    <html>
-    <br>
-    <form method="post" action="/submit_poll">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <input type="hidden" name="pid" value="{!! $options[key(reset($options))]->pid !!}" >
-        <div class=":poll-options">
-            <?php foreach($options as $index => $option): ?>
-            <div class=":poll-option">
-                <input type="radio" name="{!! ($index) !!}" value="{!! $option->oid !!}"    >
-                <label >{!! $option->option !!}</label>
-                <br>
-            </div>
-            <?php endforeach;?>
+<html>
+<br>
+<form method="post" action="/submit_poll">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <input type="hidden" name="pid" value="{!! $options[key(reset($options))]->pid !!}" >
+    <div class=":poll-options">
+        <?php foreach($options as $index => $option): ?>
+        <div class=":poll-option">
+            <input type="radio" name="{!! ($index) !!}" value="{!! $option->oid !!}"    >
+            <label >{!! $option->option !!}</label>
+            <br>
         </div>
+        <?php endforeach;?>
+    </div>
 
-        <input type="submit" value="Submit Vote">
-    </form>
-@else
-    <p>This event has no polls.</p>
-@endif
+    <input type="submit" value="Submit Vote">
+</form>
 
