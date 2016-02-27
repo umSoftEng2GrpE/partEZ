3403a51387ed3131eb789dbcc3f88d28d43bb152
Greg Joubert <umjoubeg@myumanitoba.ca>
2016-02-26 19:50:00 -0600

    return different views depending on creator status

--- partEZ/resources/views/events/event_details.blade.php
+++ partEZ/resources/views/events/event_details.blade.php
@@ -40,14 +40,20 @@
                 	</div>
                 	<div class="tab-pane" id="tab2">
                 		<div class="event-details">
-                		<h4>Dates</h4>
-							@if (count($all_options))
-								@foreach($all_options as $options)
-								@include('polls.poll_display', $options )
-								@endforeach
-							@else
-							<p>This event has no polls.</p>
-							@endif
+                            <h4>Dates</h4>
+                            @if (count($all_options))
+                                @if ($event['uid'] == Auth::user()->uid)
+                                    @foreach($all_options as $options)
+                                        @include('polls.poll_display', $options )
+                                    @endforeach
+                                @else
+                                    @foreach($all_options as $options)
+                                        @include('polls.poll_vote', $options )
+                                    @endforeach
+                                @endif
+                            @else
+                                <p>This event has no polls.</p>
+                            @endif
 						</div>
                 	</div>
                 	<div class="tab-pane" id="tab3">
