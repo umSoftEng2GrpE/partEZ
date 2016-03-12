16f8602407e5f3f740610e1863deee38594e6a06
Delroy <delroy.hiebert@gmail.com>
2016-03-11 20:09:03 -0600

    fixed button wording

--- partEZ/resources/views/tab_detail/item_list.blade.php
+++ partEZ/resources/views/tab_detail/item_list.blade.php
@@ -3,7 +3,7 @@
     @if( count($items_list))
         @foreach($items_list as $item)
             @if( $item->uid == 0 )
-                <p>{{ $item->description }} <a class='btn btn-xs btn-info' href="{!! route('assign_user', [$item->iid, 'eid'=>$event['eid']]) !!}">Testing</a></p>
+                <p>{{ $item->description }} <a class='btn btn-xs btn-info' href="{!! route('assign_user', [$item->iid, 'eid'=>$event['eid']]) !!}">Bring this!</a></p>
                 @else
                         <!--TODO:Optimize this process-->
                 @foreach($item_users as $tmp_user)
