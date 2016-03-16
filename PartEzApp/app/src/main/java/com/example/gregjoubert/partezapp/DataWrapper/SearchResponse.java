e42ba081c11ef8ac92ffa7ba325c1f01d23ecc08
Greg Joubert <umjoubeg@myumanitoba.ca>
2016-03-16 14:58:46 -0500

    added a wrapper class for the array of json values

---
+++ PartEzApp/app/src/main/java/com/example/gregjoubert/partezapp/DataWrapper/SearchResponse.java
@@ -0,0 +1,14 @@
+package com.example.gregjoubert.partezapp.DataWrapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by gregjoubert on 2016-03-16.
+ */
+public class SearchResponse
+{
+    public ArrayList<Result> array;
+
+}
