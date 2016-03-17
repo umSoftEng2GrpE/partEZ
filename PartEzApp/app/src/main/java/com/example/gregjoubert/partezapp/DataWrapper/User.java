f222a976caa76b79ff46635d1e5f8d7808a97e6d
Greg Joubert <umjoubeg@myumanitoba.ca>
2016-03-16 21:00:03 -0500

    user class for users get

---
+++ PartEzApp/app/src/main/java/com/example/gregjoubert/partezapp/DataWrapper/User.java
@@ -0,0 +1,15 @@
+package com.example.gregjoubert.partezapp.DataWrapper;
+import com.google.gson.annotations.SerializedName;
+/**
+ * Created by gregjoubert on 2016-03-16.
+ */
+public class User
+{
+    public String uid;
+    public String firstname;
+    public String lastname;
+    public String email;
+    public String created_at;
+    public String updated_at;
+    public String active;
+}
